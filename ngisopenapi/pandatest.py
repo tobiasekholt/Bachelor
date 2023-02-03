b15736a6c8badce9993e7a92bf24162f50f7818a
niklasfugledal <niklasfugledal@live.no>
2023-02-03 11:57:32 +0100

    mer testing

--- ngisopenapi/pandatest.py
+++ ngisopenapi/pandatest.py
@@ -1,25 +1,33 @@
-import geopandas as gpd
 import psycopg2
+import psycopg2.extras
+import geopandas as gpd
 
-# Load the GeoJSON file into a GeoDataFrame
-gdf = gpd.read_file("C:\\Users\\nikla\\Bachelor\\Bachelor_2023\\Bachelor\\ngisopenapi\\buildings.geojson")
-
-# Connect to your PostgreSQL database
+# Connect to the PostgreSQL database
 conn = psycopg2.connect(
     host="postgresql-dev-kartai.postgres.database.azure.com",
     database="kartai_bachelor_2023",
     user="kartai_bachelor_2023@postgresql-dev-kartai",
     password="Io4$7M1e"
 )
+cursor = conn.cursor()
 
-# Create a cursor to execute SQL commands
-cur = conn.cursor()
+# Read the GeoJSON file into a GeoDataFrame
+gdf = gpd.read_file(r"C:\Users\nikla\Bachelor\Bachelor_2023\Bachelor\ngisopenapi\buildings.geojson")
 
-# Create a table in the database to store the GeoJSON data
-table_name = "geojson_table"
-gdf.to_sql(table_name, conn, if_exists='replace', index=False)
+# No need to add a properties column as it already exists in the table
 
-# Commit the changes and close the cursor and connection
+# No need to add a geom column as it already exists in the table
+
+# Create the table (if it doesn't already exist)
+create_table_sql = "CREATE TABLE IF NOT EXISTS buildings (properties json, geom geometry);"
+cursor.execute(create_table_sql)
 conn.commit()
-cur.close()
+
+# Insert the data into the table
+insert_sql = "INSERT INTO buildings (properties, geom) VALUES %s"
+psycopg2.extras.execute_values(cursor, insert_sql, [(row['properties'], row['geometry'].wkb_hex) for _, row in gdf.iterrows()])
+conn.commit()
+
+# Close the cursor and connection
+cursor.close()
 conn.close()
