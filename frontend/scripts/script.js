10e57abc0f3bec0324d5c4dece2d710a8e91cad0
inebredes1 <inemb@uia.no>
2023-03-24 08:18:41 +0100

    justeringer

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -14,3 +14,9 @@ let scrollHeight = Math.max(
       document.location.href = "./confirm.html";
     }
   }
+
+
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
